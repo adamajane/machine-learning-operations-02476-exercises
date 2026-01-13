3e07d1be8cffc9aeda32272a145ed46e4e377848
adamajane <adamajane@gmail.com>
2026-01-13 23:03:01 +0100

    Initial commit with Cookiecutter

---
+++ src/mlops_mnist_classifier/train.py
@@ -0,0 +1,10 @@
+from mlops_mnist_classifier.model import Model
+from mlops_mnist_classifier.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
